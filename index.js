f757ebc7561c857d4dd94f652750c1098a63df93
niloaydin <nilay.aydn97@gmail.com>
2022-04-15 17:29:53 +0300

    Compeleted the assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    const value = (pickUpLocation - 42)
+    if (value < 0) {
+        return (value * -1)
+    } else {
+        return value
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    const value = (someValue - 42)
+    if (value < 0) {
+        return (value * -1) * 264
+    } else {
+        return value * 264
+    }
+}
+
+function distanceTravelledInFeet(start,destination) {
+     const value = (destination - start)
+    if (value < 0) {
+        return (value * -1) * 264
+    } else {
+        return value * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let value = (destination - start) * 264
+    if (value < 0) {
+        value = (value * -1)
+    } else if (value > 0) {
+        value = value
+    }
+
+    if (value < 400) {
+        return  0
+    } else if (value < 2001) {
+        return (value - 400) * 0.02
+    } else if (value < 2501) {
+        return 25
+    } else if (value > 2500) {
+       return`cannot travel that far`
+
+    }
+}
